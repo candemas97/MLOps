6fd8530e9e662eb4a9dd65e58656141421134ac0
cargar_github <candemas97@hotmail.com>
2024-02-04 20:41:29 -0500

    Everyone contribution 🚀

--- assignment_1/app/app.py
+++ assignment_1/app/app.py
@@ -11,17 +11,21 @@ def index():
     return "MLOps: Assigment 1"
 
 
-@app.post("/preditcion_penguin")
+@app.post("/prediction_penguin")
 async def predict(new_observation: models_api.PENGUIN):
     response = prediction.assign_penguin_specie(new_observation.model_dump(), "xgb")
     return {"penguins": response}
 
-@app.post("/preditcion_penguin/select_model/{model_to_be_used}")
+
+@app.post("/prediction_penguin/select_model/{model_to_be_used}")
 async def predict(model_to_be_used: str, new_observation: models_api.PENGUIN):
     if model_to_be_used not in ["xgb", "random_forest"]:
         return {"error": f"No existe {model_to_be_used}. Selecione xgb o random_forest"}
-    response = prediction.assign_penguin_specie(new_observation.model_dump(), "xgb")
+    response = prediction.assign_penguin_specie(
+        new_observation.model_dump(), model_to_be_used
+    )
     return {"penguins": response}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8080)
