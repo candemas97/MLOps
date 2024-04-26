3eb452a30abe6abc2d1df907756b2255371d28e7
Grupo 9 <candemas97@hotmail.com>
2024-04-26 09:41:58 -0500

    Updating reading data✅

--- project_3/data/README.md
+++ project_3/data/README.md
@@ -1,1 +1,49 @@
-You must run all the jupyter notebook to take initial data. The file is called `code/read_data_source.ipynb`
+# Download Data from Source
+
+You must run the file in the following path:`/code/read_data_source.py`.
+
+To do it do the following:
+
+1. Open a new terminal
+2. Go to code within data as follows:
+
+```
+cd project_3
+cd data
+cd code
+```
+
+3. Run the following line:
+
+If yo are in `Linux`
+
+```
+python3 -m read_data_source.py
+```
+
+If you are in `Windows`
+
+```
+python -m read_data_source.py
+```
+
+Then it will create the needed data for this project.
+
+# Run the batch data
+
+If you need to have the data in a API do the following
+
+1. Open a new terminal
+2. Go to data within project_3 as follows:
+
+```
+cd project_3
+cd data
+```
+
+3. Create the container:
+
+```
+docker-compose up
+```
+
---
+++ project_3/data/code/read_data_source.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import pandas as pd
+import numpy as np
+
+## download the dataset
+# Directory of the raw data files
+_data_root = '../Diabetes'
+# Path to the raw training data
+_data_filepath = os.path.join(_data_root, 'Diabetes.csv')
+# Download data
+os.makedirs(_data_root, exist_ok=True)
+if not os.path.isfile(_data_filepath):
+    #https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/
+    url = 'https://docs.google.com/uc?export= \
+    download&confirm={{VALUE}}&id=1k5-1caezQ3zWJbKaiMULTGq-3sz6uThC'
+    r = requests.get(url, allow_redirects=True, stream=True)
+    open(_data_filepath, 'wb').write(r.content)
+
+df = pd.read_csv("../Diabetes/Diabetes.csv")
+
+train_split = 0.8973
+val_split = 0.05135
+df_provicional = df.sample(frac=1, random_state=42)
+df_train, df_validation, df_test = np.split(df_provicional, [int(train_split * len(df_provicional)), int((train_split+val_split) * len(df_provicional))])
+
+print(len(df_train))
+df_train.to_csv("../Diabetes/diabetes_train.csv")
+df_train.head()
+
+print(len(df_validation))
+df_validation.to_csv("../Diabetes/diabetes_val.csv")
+df_validation.head()
+
+print(len(df_test))
+df_test.to_csv("../Diabetes/diabetes_test.csv")
+df_test.head()
